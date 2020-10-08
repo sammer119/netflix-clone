eb5b3e194232f3ffe9d385e66c0b518df8d10654
sammer119 <sam1atkins@yahoo.co.uk>
2020-10-08 12:12:24 +0100

    push

--- src/requests.js
+++ src/requests.js
@@ -1,9 +1,9 @@
 const API_KEY = "7d1d08e5b185ca94399eaefd1f3427f8";
 
 const requests = {
-    fetchTrending: `trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `trending/all/week?api_key=${API_KEY}&language=en-UK`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-UK`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
