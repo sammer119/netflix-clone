7a4a75185eedc5c9ccbf430e6e9f4744af2e6099
sammer119 <sam1atkins@yahoo.co.uk>
2021-11-08 14:47:06 +0000

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
         <div className={`nav ${show && "nav__black"}`}>
             <img
                 className="nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/800px-Logo_Netflix.png"
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                 alt="Netflix Logo"
             />
 
